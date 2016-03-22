9fe72ad94d4b4a94956285cd22ce63de6f485e3b
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2016-03-22 12:18:32 -0500

    Marking lag as red

--- src/main/groovy/org/zirbes/kafka/burrow/domain/LagInfo.groovy
+++ src/main/groovy/org/zirbes/kafka/burrow/domain/LagInfo.groovy
@@ -1,6 +1,9 @@
 package org.zirbes.kafka.burrow.domain
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 import org.joda.time.DateTime
+import org.zirbes.kafka.burrow.util.Colorizer
 
 class LagInfo {
 
@@ -13,11 +16,15 @@ class LagInfo {
     }
 
     String toString() {
-        List<String> info = []
-        if (lag) { info << "lag: ${lag}".toString() }
+        Integer padding = 60
+        List<String> info = new CopyOnWriteArrayList<String>()
+        if (lag) {
+            //padding += Colorizer.RED.length() + Colorizer.RESET.length()
+            info << 'lag: ' + Colorizer.red(lag.toString())
+        }
         if (offset) { info << "offset: ${offset}".toString() }
         if (lag || offset) {
-            info << 'last seen: ' + time.toString('M/d h:mm:ss a z')
+            info << 'seen: ' + time.toString('M/d h:mm:ss a z')
         }
         return info.join(', ').padLeft(48)
     }
